adc9e265a47241e741f5b54769b61fa888efb79d
inahyu <inahyu9331@gmail.com>
2024-06-11 13:38:26 +0900

    [유인아] ERD DROP table 수정

--- ERD/table_ctt_drop.sql
+++ ERD/table_ctt_drop.sql
@@ -1,4 +1,20 @@
-
+DROP TABLE IF EXISTS ctt_cart;
+DROP TABLE IF EXISTS ctt_bookmark;
+DROP TABLE IF EXISTS ctt_review;
+DROP TABLE IF EXISTS ctt_order_item;
+DROP TABLE IF EXISTS ctt_order;
 DROP TABLE IF EXISTS ctt_user;
+DROP TABLE IF EXISTS ctt_calender;
 DROP TABLE IF EXISTS ctt_authority;
 DROP TABLE IF EXISTS ctt_menu;
+
+
+
+
+
+
+
+
+
+
+
