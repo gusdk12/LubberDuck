0ebd4ee9b35c3d9d854b91f227f69bc4d8888a98
dalyeon2 <woohyun2228@gmail.com>
2024-06-15 12:04:29 +0900

    [김다현] 캘린더 테이블 이름 수정

--- ERD/table_ctt_drop.sql
+++ ERD/table_ctt_drop.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS ctt_review;
 DROP TABLE IF EXISTS ctt_order_item;
 DROP TABLE IF EXISTS ctt_order;
 DROP TABLE IF EXISTS ctt_user;
-DROP TABLE IF EXISTS ctt_calender;
+DROP TABLE IF EXISTS ctt_calendar;
 DROP TABLE IF EXISTS ctt_authority;
 DROP TABLE IF EXISTS ctt_menu;
 
