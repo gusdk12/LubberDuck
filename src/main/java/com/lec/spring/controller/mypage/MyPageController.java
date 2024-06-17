945bee511066a553802d7733564fc7c6223de893
inahyu <inahyu9331@gmail.com>
2024-06-17 18:35:16 +0900

    [유인아] myPageUpdate 수정 데이터 저장완료

--- src/main/java/com/lec/spring/controller/mypage/MyPageController.java
+++ src/main/java/com/lec/spring/controller/mypage/MyPageController.java
@@ -86,6 +86,9 @@ public class MyPageController {
         if (result.hasErrors()){
             redirectAttrs.addFlashAttribute("nickname", user.getNickname());
             redirectAttrs.addFlashAttribute("email", user.getEmail());
+            redirectAttrs.addFlashAttribute("year", year);
+            redirectAttrs.addFlashAttribute("month", month);
+            redirectAttrs.addFlashAttribute("day", day);
 
             List<FieldError> errList = result.getFieldErrors();
             for(FieldError err : errList){
@@ -97,7 +100,14 @@ public class MyPageController {
 
         user.setBirth_date(LocalDate.of(year, month, day));
 
-        model.addAttribute("result", userService.update(user));
+        int updateResult = userService.update(user);
+
+        if (updateResult > 0) {
+            model.addAttribute("success", true);
+        } else {
+            model.addAttribute("success", false);
+        }
+
         return "mypage/myPageUpdateOk";
     }
 
