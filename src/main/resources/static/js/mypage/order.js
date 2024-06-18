92a92e578d56c055d74d91862655641cdb3cc9f9
taeining <taeining00@gmail.com>
2024-06-18 15:29:36 +0900

    [이다혜] mypage order html DB연동

--- src/main/resources/static/js/mypage/order.js
+++ src/main/resources/static/js/mypage/order.js
@@ -6,4 +6,59 @@ $(document).ready(function(){
         'color' : '#54320f',
         'font-weight': 'bold'
     });
+
+    buildBody();
 });
+
+function buildBody(){
+    orderList.forEach(order => {
+        let itemsHTML = "";
+        let totalPrice = 0;
+        orderItemMap[order.id].forEach(item => {
+            itemsHTML += `
+                <div class="items">
+                    <div class="item">
+                        <span class="item-name">${item.menu.name}</span>
+                        <span class="quantity">${item.quantity}</span>
+                        <span class="item-price" >${item.price}</span>
+                        <span>
+                            <input type="button" value="리뷰작성" name="reviewBtn" onclick="location.href='/review/write'">
+                        </span>
+                    </div>
+                </div>`;
+            totalPrice += (item.price * item.quantity);
+        });
+
+        $('.container').append(`
+            <div class="receipt">
+            <div class="background">
+                <img src="/img/mypage/receipt.png">
+                </div>
+                <div class="content">
+                    <h2 class="shopname">LubberDuck</h2>
+                    <div class="header-info">
+                        <p class="name">${user.nickname}님</p>
+                        <p class="date-time">${order.regdate}</p>
+                    </div>
+                    <p class="order-nm">주문번호: ${order.id}</p>
+                    <div class="dashed"></div>
+                    <div class="item-header">
+                        <span class="menuName">&lt;메뉴이름&gt;</span>
+                        <span class="quantity">&lt;주문수량&gt;</span>
+                        <span class="menuPrice">&lt;메뉴가격&gt;</span>
+                        <span class="review">&lt;리뷰작성&gt;</span>
+                    </div>
+                    <div class="dashed"></div>
+                    ${itemsHTML}
+                    <div class="dashed"></div>
+                    <div class="totals">
+                        <div class="total-item">
+                            <span class="total-price"></span>
+                            <span>${totalPrice}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `);
+    });
+}
