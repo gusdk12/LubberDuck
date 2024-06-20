e359eec2c45b83bbd413dd46a65fe505711a7071
inahyu <inahyu9331@gmail.com>
2024-06-20 17:15:57 +0900

    [유인아] 상세페이지에서 리뷰데이터 불러오기 성공

--- src/main/java/com/lec/spring/controller/menu/MenuController.java
+++ src/main/java/com/lec/spring/controller/menu/MenuController.java
@@ -4,13 +4,17 @@ import com.lec.spring.domain.QryResult;
 import com.lec.spring.domain.User;
 import com.lec.spring.domain.cart.QryCartList;
 import com.lec.spring.domain.menu.Menu;
+import com.lec.spring.domain.review.Review;
 import com.lec.spring.service.menu.MenuService;
+import com.lec.spring.service.review.ReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/menu")
@@ -19,11 +23,21 @@ public class MenuController {
     @Autowired
     private MenuService menuService;
 
+    @Autowired
+    private ReviewService reviewService;
+
     @GetMapping("/detail/{menu_id}")
     public String detail(@PathVariable Long menu_id, Model model){
         Menu menu = menuService.findById(menu_id);
+        if (menu == null) {
+            // 처리할 내용 추가
+        }
+
         model.addAttribute("menu", menu);
         model.addAttribute("menuList", menuService.sequenceList());
+
+        List<Review> reviews = reviewService.findByItemMenu(menu_id);
+        model.addAttribute("reviews", reviews);
         return "menu/detail";
     }
 
