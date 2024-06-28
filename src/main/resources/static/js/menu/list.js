dc479baa21f91fce64c8d9d670a4a66ccc433554
Yushin <dbtls6513@naver.com>
2024-06-28 16:30:25 +0900

    [송유신] 오늘의 메뉴 디자인 수정

--- src/main/resources/static/js/menu/list.js
+++ src/main/resources/static/js/menu/list.js
@@ -67,7 +67,7 @@ async function appendMenuToPlate() {
                     <div id="leftwreath"></div> 
                     <div id="rightwreath"></div> 
                     <div id="sunburst"></div> 
-                    <div id="todaylabel"></div> 
+                    <img id="todaylabel">
 <!--                    <div class="starRightEffect" style="transform: translate(300px, 350px) rotate(30deg)"><div class="starline"></div><div class="rotatestar"></div></div>-->
 <!--                    <div class="starRightEffect" style="transform: translate(320px, 275px)"><div class="starline"></div><div class="rotatestar"></div></div>-->
 <!--                    <div class="starRightEffect" style="transform: translate(300px, 200px) rotate(-30deg)"><div class="starline"></div><div class="rotatestar"></div></div>-->
@@ -179,6 +179,13 @@ function openMenu() {
 
     let menu = document.getElementById("menu");
     menu.style.width = `${paperCount * 500}px`;
+    menu.addEventListener('transitionend', function(event) {
+        if (event.propertyName === 'width') {
+            let todaylabel = document.getElementById("todaylabel");
+            (todaylabel) && (todaylabel.style.width = `300px`);
+        }
+    });
+
 
     const menuBody = document.querySelector('#menuBody');
     const menuscroll = document.querySelector('#menuscroll');
