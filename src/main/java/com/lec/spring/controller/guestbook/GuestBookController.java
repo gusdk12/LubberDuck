572fd4c312aa4f3d186216b8e5e64532662fb79a
gusdk12 <hyuna3384@naver.com>
2024-06-28 15:37:28 +0900

    [신현아] guestbook 이미지 추가

--- src/main/java/com/lec/spring/controller/guestbook/GuestBookController.java
+++ src/main/java/com/lec/spring/controller/guestbook/GuestBookController.java
@@ -1,5 +1,11 @@
 package com.lec.spring.controller.guestbook;
 
+import com.lec.spring.domain.User;
+import com.lec.spring.service.UserService;
+import com.lec.spring.service.guestBook.GuestBookService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,10 +15,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/")
 public class GuestBookController {
 
+    @Autowired
+    private GuestBookService guestBookService;
+
+    @Autowired
+    private UserService userService;
+
     @GetMapping("/guestbook")
-    public void guestbook(Model model){
-
-
+    public void guestbook(Model model,
+                          @AuthenticationPrincipal UserDetails userDetails){
+        model.addAttribute("postItAll", guestBookService.postItAll());
     }
 
 
