8a6062851cc6a1e4481b33126cae9013bbfc8d83
Yushin <dbtls6513@naver.com>
2024-06-28 14:53:24 +0900

    [송유신] 방명록 드래그버그 수정

--- src/main/resources/static/js/guestbook/guestbook.js
+++ src/main/resources/static/js/guestbook/guestbook.js
@@ -1,13 +1,54 @@
 
 window.addEventListener('load', () => {
+    initBoardDragEvent();
     initPostItCreateEvent();
 });
 
+function initBoardDragEvent() {
+    let isDragging = false;
+    let startX, startY, scrollLeft, scrollTop;
+    //const allcontainer = document.querySelector('#allcontainer');
+
+    document.addEventListener('contextmenu', function(e) {
+        e.preventDefault(); // Prevent the default context menu from appearing
+    });
+
+    document.addEventListener('mousedown', function(e) {
+        if (e.button === 2) { // Right mouse button
+            isDragging = true;
+            startX = e.clientX;
+            startY = e.clientY;
+            scrollLeft = window.scrollX;
+            scrollTop = window.scrollY;
+            document.body.style.cursor = 'grabbing';
+        }
+    });
+
+    document.addEventListener('mousemove', function(e) {
+        if (isDragging) {
+            const dx = e.clientX - startX;
+            const dy = e.clientY - startY;
+            // if(scrollTop - dy > 0)
+            //     return;
+
+            window.scrollTo(scrollLeft - dx, scrollTop - dy);
+            // allcontainer.style.left = `${scrollLeft - dx}px`;
+            // allcontainer.style.top = `${scrollTop - dy}px`;
+            // console.log("left " + (scrollLeft - dx));
+            // console.log("top " + (scrollTop - dy));
+        }
+    });
+
+    document.addEventListener('mouseup', function(e) {
+        if (isDragging) {
+            isDragging = false;
+            document.body.style.cursor = 'grab';
+        }
+    });
+}
 function initPostItCreateEvent(){
-    // const draggable = document.getElementById('postIt1');
     const draggableList = document.querySelectorAll('.createpostIt');
     const dropzone = document.querySelector('#guestbook');
-    let clone = null;
     let offsetX, offsetY;
 
     for(let draggable of draggableList) {
@@ -26,8 +67,8 @@ function initPostItCreateEvent(){
             function moveAt(pageX, pageY) {
                 const dropzoneRect = dropzone.getBoundingClientRect();
 
-                let newLeft = (pageX - offsetX) - dropzoneRect.left;
-                let newTop = (pageY - offsetY) - dropzoneRect.top;
+                let newLeft = (pageX - offsetX) - dropzoneRect.left - window.scrollX;
+                let newTop = (pageY - offsetY) - dropzoneRect.top - window.scrollY;
 
                 (newLeft < 0) && (newLeft = 0);
                 (newLeft > (dropzoneRect.width - newDiv.offsetWidth)) && (newLeft = dropzoneRect.width - newDiv.offsetWidth);
@@ -48,6 +89,10 @@ function initPostItCreateEvent(){
                 document.removeEventListener('mousemove', onMouseMove);
                 newDivonmouseup = null;
                 newDiv.style.zIndex = 1; // TODO
+
+                console.log("x-coordinate : " + newDiv.offsetLeft);
+                console.log("y-coordinate : " + newDiv.offsetTop);
+                console.log("z-coordinate : " + 1);
             };
         });
     }
