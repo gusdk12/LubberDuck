20a71598a3e57b1d60e458c26ab233f16346b1b0
Yushin <dbtls6513@naver.com>
2024-07-03 13:39:48 +0900

    [송유신] 채팅 버그 수정

--- src/main/java/com/lec/spring/controller/chat/ChatController.java
+++ src/main/java/com/lec/spring/controller/chat/ChatController.java
@@ -68,8 +68,8 @@ public class ChatController {
         else menuProm = "현재 아쉽게도 메뉴판에는 메뉴가 하나도 없습니다. 손님에게 메뉴가 비어있다고 양해를 구하세요.";
 
         String initProm =  "당신은 'Rubber Bar'의 바텐더이자 훌륭한 상담가입니다. " +
-                menuProm + "칵테일 이름을 한국어로 번역하지 마세요!!" +
-                "손님의 기분을 좋아지게 해주기 위한 대화를 하세요. 손님 대신 대답하지 마세요!!! 손님을 사칭하지 마세요!!! 손님과 적절한 대화를 이어나가세요. 손님이 칵테일 추천을 더이상 바라지 않는다면, 추천하지 마세요. 말을 반복하지 마세요!! 이미 손님에게 또 인사하지 마세요!!";
+                menuProm + "칵테일 이름을 한국어로 번역하지 마세요!! 대답은 한국어로 하세요." +
+                "손님의 기분을 좋아지게 해주기 위한 대화를 하세요. 손님 대신 대답하지 마세요!!! 손님을 사칭하지 마세요!!! 손님과 적절한 대화를 이어나가세요. 손님이 칵테일 추천을 더이상 바라지 않는다면, 추천하지 마세요. 말을 반복하지 마세요!! 손님에게 인사하지 마세요!! 환영한다고 하지 마세요!!";
         String exampleProm = "바텐더 답장 예시입니다. : '오, 그건 'Old Fashioned'입니다. 올드 패션드는 가장 고전적인 칵테일 중 하나로, 위스키의 풍미와 비터의 조화가 훌륭한 균형을 이룹니다. 간단한 재료로 만들어지지만, 세심한 기술과 정성이 필요한 칵테일이기도 하죠.'" +
                 ", '저는 바텐더이지만, 상담가이기도 합니다. 오늘은 어떤 기분이 드시나요?'" +
                 ", '물론이죠, 여기서는 훌륭한 칵테일뿐만 아니라 마음의 평화도 찾으실 수 있습니다. 불안과 두려움을 잠시 잊고 편안한 밤을 보내는 데에 도움을 드리고 싶군요.'" +
@@ -82,7 +82,7 @@ public class ChatController {
         QryChatList allChat = chatServiceImpl.findByUser(user_id, menuService.sequenceList());
 
         List<String> chatList = new ArrayList<>();
-        allChat.getList().forEach(chat -> chatList.add(chat.getContent()));
+        allChat.getList().forEach(chat -> chatList.add(chat.getRole() + ": " + chat.getContent()));
         Collections.reverse(chatList);
 
         String result = ChatManager.getInstance().getBartendarSetting() +
